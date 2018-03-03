108738f6c19f67b161bf471177396bd82b233b2c
MTH <thornvik.mans@gmail.com>
2018-03-03 13:59:29 +0100

    Added verbose message for missing env var

--- SEALS/camera/picture_storage.py
+++ SEALS/camera/picture_storage.py
@@ -4,7 +4,7 @@ from os import path, getenv
 def get_picture_directory():
     user = getenv('DJANGO_STATIC_DIR', '~/')
     if user == '~/':
-        raise EnvironmentError
+        raise EnvironmentError('You do not seem to have installed the project correctly, environment variable missing.')
 
     p = path.expanduser(user)
     return p + '/alarm_pictures/'

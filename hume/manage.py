db24b3c84ceb467571bdd7259402b333394b4c3e
MTH <thornvik.mans@gmail.com>
2019-05-29 18:01:25 +0200

    Added new module structure

--- hume/manage.py
+++ hume/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'http_server.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'http.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

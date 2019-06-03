932b57f1b01873148c9899327e686ca20c864ec6
MTH <thornvik.mans@gmail.com>
2019-06-03 14:15:56 +0200

    Added new django server

--- hume/transport/http/server/manage.py
+++ hume/transport/http/server/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'http.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'http_django_server.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

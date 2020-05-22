ae42cbb323065d8f36813ab784e15c8914411889
MTH <thornvik.mans@gmail.com>
2020-05-22 20:26:43 +0200

    Maybe don't need much in terms of state storage?

--- hume/device_controller/device_controller/device/model.py
+++ hume/device_controller/device_controller/device/model.py
@@ -7,18 +7,25 @@ from device_controller.util.storage import PersistentModel
 
 class Device(PersistentModel):
 
+    # HUME only needs connection information for the device, not information
+    # about sub-devices (if compound). Sub-device info could be useful for
+    # validating incoming requests, but if proper authentication and
+    # authorization is in place, each request should anyway come from a solid
+    # source with enough knowledge about device structures.
     uuid = peewee.CharField()
     ip_address = peewee.CharField()
 
+    attached = peewee.BooleanField()
 
-class DeviceStatus:
 
-    key: int  # Device ID
-
-    heartbeat: datetime
-    state: str
-
-    def __init__(self, key, heartbeat=None, state=None):
-        self.key = key
-        self.heartbeat = heartbeat
-        self.state = state
+# class DeviceStatus:
+#
+#     key: int  # Device ID
+#
+#     heartbeat: datetime
+#     state: str
+#
+#     def __init__(self, key, heartbeat=None, state=None):
+#         self.key = key
+#         self.heartbeat = heartbeat
+#         self.state = state

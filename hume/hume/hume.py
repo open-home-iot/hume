6037154978dc7e9fd5656734c241b20e2024ef83
Måns Thörnvik <thornvik.mans@gmail.com>
2022-01-17 21:03:03 +0100

    Hume calls each apps lifecycle hooks on start/stop

--- hume/hume/hume.py
+++ hume/hume/hume.py
@@ -1,2 +1,31 @@
+from app.device import Device
+from app.hint import Hint
+
+
 class Hume:
-    pass
+
+    def __init__(self):
+        self.device = Device()
+        self.hint = Hint()
+
+    def start(self):
+        """Starts the HUME."""
+        self.device.pre_start()
+        self.hint.pre_start()
+
+        self.device.start()
+        self.hint.start()
+
+        self.device.post_start()
+        self.hint.post_start()
+
+    def stop(self):
+        """Stops the HUME."""
+        self.device.pre_stop()
+        self.hint.pre_stop()
+
+        self.device.stop()
+        self.hint.stop()
+
+        self.device.post_stop()
+        self.hint.post_stop()
